1193d246587d4beb5ea3e95d1799333bb3687ffa
paul-juang <pauljuang3@gmail.com>
2020-03-26 09:54:43 +0800

    update num539/649/prenum539/649.js

--- public/js/prenum649.js
+++ public/js/prenum649.js
@@ -1,5 +1,73 @@
 let preloto649 = [  
 
+  {date: "2020/03/20", 
+  summary: [
+  {num:'01',diff:-1,   mindiff:-1,  maxdiff:-4,  intv:1,   p:0.1292},
+  {num:'02',diff:-2,   mindiff:-5,  maxdiff:-3,  intv:7,   p:0.6203},
+  {num:'03',diff:3,    mindiff:0,   maxdiff:-1,  intv:14,  p:0.8558},
+  {num:'04',diff:-4,   mindiff:-4,  maxdiff:-6,  intv:4,   p:0.4251},
+  
+  {num:'05',diff:-3,   mindiff:-4,  maxdiff:-6,  intv:13,  p:0.8344},
+  {num:'06',diff:-5,   mindiff:-4,  maxdiff:-8,  intv:18,  p:0.9171},
+  {num:'07',diff:0,    mindiff:1,   maxdiff:11,  intv:11,  p:0.7816},
+  
+  {num:'08',diff:0,   mindiff:3,    maxdiff:3,   intv:1,   p:0.1292},
+  {num:'09',diff:3,   mindiff:-5,   maxdiff:-3,  intv:7,   p:0.6203},
+  {num:'10',diff:1,   mindiff:0,    maxdiff:-2,  intv:2,   p:0.2417},
+  
+  {num:'11',diff:-5,  mindiff:-4,   maxdiff:-1,  intv:6,   p:0.5639},
+  {num:'12',diff:2,   mindiff:9,    maxdiff:12,  intv:4,   p:0.4251},
+  {num:'13',diff:4,   mindiff:3,    maxdiff:2,   intv:1,   p:0.1292},
+  
+  {num:'14',diff:-3,  mindiff:-1,   maxdiff:-2,  intv:3,   p:0.3396},
+  {num:'15',diff:2,   mindiff:-1,   maxdiff:-3,  intv:5,   p:0.4993},
+  {num:'16',diff:3,   mindiff:1,    maxdiff:1,   intv:5,   p:0.4993},
+  
+  {num:'17',diff:0,   mindiff:1,    maxdiff:-1,  intv:2,   p:0.2417},
+  {num:'18',diff:3,   mindiff:2,    maxdiff:-4,  intv:7,   p:0.6203},
+  {num:'19',diff:-2,  mindiff:1,    maxdiff:-2,  intv:13,  p:0.8344},
+  {num:'20',diff:-2,  mindiff:3,    maxdiff:4,   intv:16,  p:0.8906},
+  
+  {num:'21',diff:1,   mindiff:1,    maxdiff:1,   intv:24,  p:0.9638},
+  {num:'22',diff:2,   mindiff:1,    maxdiff:0,   intv:3,   p:0.3396},
+  {num:'23',diff:3,   mindiff:0,    maxdiff:-1,  intv:11,  p:0.7816},
+  
+  {num:'24',diff:-4,  mindiff:-2,   maxdiff:-6,  intv:18,  p:0.9171},
+  {num:'25',diff:-4,  mindiff:-10,  maxdiff:-11, intv:4,   p:0.4251},
+  {num:'26',diff:1,   mindiff:-1,   maxdiff:-2,  intv:16,  p:0.8906},
+  
+  {num:'27',diff:-3,  mindiff:-6,   maxdiff:-7,  intv:7,   p:0.6203},
+  {num:'28',diff:0,   mindiff:-3,   maxdiff:-5,  intv:2,   p:0.2417},
+  {num:'29',diff:-5,  mindiff:-3,   maxdiff:-7,  intv:2,   p:0.2417},
+  {num:'30',diff:1,   mindiff:1,    maxdiff:0,   intv:5,   p:0.4993},
+  
+  {num:'31',diff:-2,   mindiff:0,    maxdiff:-2,  intv:10,   p:0.7493},
+  {num:'32',diff:-2,  mindiff:2,    maxdiff:4,   intv:3,   p:0.3396},
+  {num:'33',diff:-1,   mindiff:-1,   maxdiff:-3,  intv:3,   p:0.3396},
+  
+  {num:'34',diff:-2,   mindiff:-4,   maxdiff:2,   intv:3,   p:0.3396},
+  {num:'35',diff:1,   mindiff:-1,   maxdiff:-3,  intv:9,   p:0.7121},
+  {num:'36',diff:-2,  mindiff:1,    maxdiff:2,   intv:11,  p:0.7816},
+  
+  {num:'37',diff:3,   mindiff:7,    maxdiff:10,  intv:4,   p:0.4251},
+  {num:'38',diff:2,   mindiff:3,    maxdiff:-1,  intv:20,  p:0.9371},
+  {num:'39',diff:-3,  mindiff:-2,   maxdiff:-7,  intv:9,   p:0.7121},
+  {num:'40',diff:-3,  mindiff:0,    maxdiff:-4,  intv:26,  p:0.9725},
+
+  {num:'41',diff:-1,   mindiff:2,    maxdiff:9,   intv:18,  p:0.9171},
+  {num:'42',diff:0,   mindiff:2,    maxdiff:2,   intv:6,   p:0.5639},
+  {num:'43',diff:-4,  mindiff:-4,   maxdiff:-4,  intv:1,   p:0.1292},
+  
+  {num:'44',diff:-3,  mindiff:-8,   maxdiff:-9,  intv:8,   p:0.6693},
+  {num:'45',diff:0,   mindiff:5,    maxdiff:-1,  intv:21,  p:0.9452},
+  {num:'46',diff:-3,  mindiff:-4,   maxdiff:-2,  intv:1,   p:0.1292},
+  
+  {num:'47',diff:0,   mindiff:0,    maxdiff:-2,  intv:2,   p:0.2417},
+  {num:'48',diff:1,   mindiff:6,    maxdiff:1,   intv:1,   p:0.1292},
+  {num:'49',diff:1,   mindiff:-2,   maxdiff:-3,  intv:3,   p:0.3396}
+  ] },
+  
+
   {date: "2020/03/17", 
   summary: [
   {num:'01',diff:-1,   mindiff:-2,  maxdiff:-5,  intv:3,   p:0.3396},
@@ -65,9 +133,9 @@ let preloto649 = [
   {num:'47',diff:1,   mindiff:0,    maxdiff:-2,  intv:1,   p:0.1292},
   {num:'48',diff:1,   mindiff:5,    maxdiff:0,   intv:6,   p:0.5639},
   {num:'49',diff:2,   mindiff:-2,   maxdiff:-3,  intv:2,   p:0.2417}
-
-
   ] }
+
+
 
 
   ];
