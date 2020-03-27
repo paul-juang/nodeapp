500ed00c23b7859d9b40cba0428ebe576fe83a14
paul-juang <pauljuang3@gmail.com>
2020-03-27 14:20:57 +0800

    update for nodeapp app

--- public/js/suloto539.js
+++ public/js/suloto539.js
@@ -1,3 +1,4 @@
+
 //print winning numbers statistic summary
 $(function() {
   let filterarr = loto539.filter(function(obj) {
@@ -19,6 +20,8 @@ $(function() {
   renderTable(arrofobj);
 
 })
+
+
 function renderTable(objarr) {
   let begdate = objarr[0].date;
   let yyyyb = begdate.substr(0,4);
@@ -31,7 +34,7 @@ function renderTable(objarr) {
   let dde = enddate.substr(8,2);
   enddate = yyyye + "/" + mme + "/" + dde;
   let dateperiod = enddate + " - " + begdate;
-  $("<h4>").text("今彩539頭獎總摘要").css({textAlign: "center",fontWeight:"bold"})
+  $("<h4>").text("今彩539中獎統計").css({textAlign: "center",fontWeight:"bold"})
   .appendTo('body');
   $("<h5>").text(dateperiod).css({textAlign: "center",fontWeight:"bold"})
   .appendTo('body');
@@ -109,8 +112,98 @@ function renderTable(objarr) {
        )              
       .appendTo(tbody);
     })
-  $("<br>").appendTo('body');
+  //$("<br>").appendTo('body');
 
   })
 
 }
+
+//print UL winning numbers statistic summary
+
+$(function() {
+  let filterarr = loto539.filter(function(obj) {
+    return obj["summary"];
+  });
+  
+  let arrofnum539 = filterarr.reduce((reduceNum,numobj)=> {
+    let temparr = [];
+
+    let date = numobj.date.substr(6,5);
+    let summary = numobj.summary;
+    temparr.push(date,summary);
+    reduceNum.push(temparr)
+    return reduceNum;
+  },[]);
+
+
+  renderUl(arrofnum539)
+
+});
+
+function renderUl(arrofnum539) {  
+
+  let numArr = [],max = 40;
+  for (let i = 1; i < max ; i++) {
+    let n = i;
+    if (n < 10) { 
+     n = "0" + n;
+   }else {
+     n = String(n);
+   }
+   numArr.push(n);
+ }
+
+console.log("numArr:",numArr);
+let uiArr = numArr.reduce(function(numarr,cn) {
+
+              let count = 0;
+              let datearr = [];
+
+              arrofnum539.forEach((arr)=> {
+                 let date = arr[0];
+                 let summary = arr[1];
+
+
+                 summary.forEach((obj)=>{
+                   if (obj.num === cn) {
+                    count += 1;
+                    datearr.push(date);
+                   }
+                 })
+ 
+              })
+              let temparr = [];  
+              temparr.push(cn,count,datearr);
+              numarr.push(temparr)
+              return numarr;
+
+},[])
+
+
+console.log("uiArr:",uiArr);
+
+$("<br>").appendTo('body');
+
+$("<label>").css({fontWeight:"bold",color:"blue"}).text("備註:").appendTo('body');
+
+$("<ul>").attr({id: "ul"}).appendTo('body');
+
+uiArr.forEach((arr)=>{
+  let num = arr[0];
+  let count = arr[1];
+  let date = arr[2].join(',');
+  
+    if (count < 10) {
+      ccount = String(count) + "\u2002";
+    }else{
+      ccount = String(count);
+    }
+
+    let mtxt = num + "  次數:  " + ccount  + "  日期: " + date;
+      
+    $("<li>").css({fontWeight:"bold",color:"blue"}).text(mtxt).appendTo($("#ul"));
+
+  })
+
+} //end of renderUl
+
