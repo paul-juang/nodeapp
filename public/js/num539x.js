24708ffc53b88ca6c146fd1e03aa64718bb50c36
paul-juang <pauljuang3@gmail.com>
2020-03-27 14:51:46 +0800

    update num539x,js

--- public/js/num539x.js
+++ public/js/num539x.js
@@ -7,8 +7,8 @@ summary: [
 {num:'08',diff:0,  mindiff:-8,   maxdiff:-9,  intv:2, p:0.2523},
 {num:'15',diff:-4,  mindiff:-13,   maxdiff:-15,   intv:18,p:0.9271},
 {num:'18',diff:-1,  mindiff:-10,  maxdiff:-10, intv:13,p:0.8489},
-{num:'35',diff:-1,   mindiff:-3,  maxdiff:-5, intv:25, p:0.9694},
-{num:'36',diff:-2,  mindiff:-11,   maxdiff:-12,  intv:25, p:0.9694},
+{num:'35',diff:-1,   mindiff:-3,  maxdiff:-5, intv:25, p:0.9736},
+{num:'36',diff:-2,  mindiff:-11,   maxdiff:-12,  intv:25, p:0.9736},
 {num:'38',diff:-3,  mindiff:-3,   maxdiff:-2,   intv:8,p:0.6875},
 {num:'39',diff:0,  mindiff:-1,  maxdiff:-2, intv:6,p:0.5821}
 ] }
