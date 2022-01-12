ee5536ec271b6ffee49333bfaa381d001157f809
paul-juang <pauljuang3@gmail.com>
2022-01-12 13:57:58 +0800

    update test649.js

--- public/js/test649.js
+++ public/js/test649.js
@@ -50,10 +50,77 @@ let reduceArr = summaryArr.reduce((sumObj,arr) => {
 }, {})
 
 console.log("reduceArr", reduceArr)
+let keyarr = Object.keys(reduceArr["01"]['2.diff'])
+.map(key => {
+	return `
+	  <li>${key}:${reduceArr["01"]['2.diff'][key]["count"]}次</li>
+      `
+}).join('')
+console.log("keyarr", keyarr)
 
-let sortarr = Object.keys(reduceArr)
-.sort((a,b) => a-b)
-.forEach( key => console.log(`
-	${key}:${reduceArr[key]["1.count"]}次
-	`))
+displayDiv(reduceArr)
 })
+
+function displayDiv(reduceArr) {
+ let sortedArr = Object.keys(reduceArr).sort((a,b) => a-b)	
+ $('<div>').attr({class:"container"})
+  .html(`
+      <ul>
+        ${sortedArr.map(num => {
+        	return `
+              <li>號碼:${num} 共出現${reduceArr[num]["1.count"]}次
+                 <ul>
+                   <li>diff:
+                      <ul>
+                        ${Object.keys(reduceArr[num]["2.diff"])
+                          .map(key=>{
+                          	return `
+                          	  <li>${key}: ${reduceArr[num]["2.diff"][key]["count"]}次</li>
+                          	`
+                          }).join('')
+                        }
+                      </ul>
+                   </li>
+                   <li>mindiff:
+                      <ul>
+                        ${Object.keys(reduceArr[num]["3.mindiff"])
+                          .map(key=>{
+                            return `
+                              <li>${key}: ${reduceArr[num]["3.mindiff"][key]["count"]}次</li>
+                            `
+                          }).join('')
+                        }
+                      </ul>
+                   </li>
+                   <li>maxdiff:
+                      <ul>
+                        ${Object.keys(reduceArr[num]["4.maxdiff"])
+                          .map(key=>{
+                            return `
+                              <li>${key}: ${reduceArr[num]["4.maxdiff"][key]["count"]}次</li>
+                            `
+                          }).join('')
+                        }
+                      </ul>
+                   </li>
+                   <li>intv:
+                      <ul>
+                        ${Object.keys(reduceArr[num]["5.intv"])
+                          .map(key=>{
+                            return `
+                              <li>${key}: ${reduceArr[num]["5.intv"][key]["count"]}次</li>
+                            `
+                          }).join('')
+                        }
+                      </ul>
+                   </li>
+                 </ul>
+              </li>
+        	`
+        }).join('')}
+
+      </ul>
+  	`)
+   .appendTo('body')
+}
+
