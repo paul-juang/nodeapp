9c09d2a7287acf54e148c271931fcac14c3c0a9f
paul-juang <pauljuang3@gmail.com>
2022-08-29 23:59:00 +0800

    update nxtloto649.js

--- public/js/nxtloto649.js
+++ public/js/nxtloto649.js
@@ -29,7 +29,7 @@ $(function() {
 
     let prelotonum = []
     if (prevfile.length > 0 ) 
-      prelotonum = prevfile[(prevfile.length)-1]["lotonum"]
+      prelotonum = prevfile[(prevfile.length)-1]["lotonum"].sort((a,b) => a-b)
     let arrOnChange = loto649.filter(obj => obj["date"] <= $("#selectdate").val())
     let baseArr = arrOnChange.slice(1,arrOnChange.length)
     let basefilerarr = baseArr.filter(obj => obj["summary"])
@@ -62,9 +62,24 @@ $(function() {
       let mindiff = objmindiff[num]["deviation"];
       let maxdiff = objmaxdiff[num]["deviation"];
       let diffpcnt = 0,mindiffpcnt = 0,maxdiffpcnt = 0,intvpcnt = 0 
+      /*if (num === prelotonum[0] || num === prelotonum[1] || num === prelotonum[2] || 
+          num === prelotonum[3] || num === prelotonum[4] || num === prelotonum[5] ) {
+        getzprobability(reduceArr, num, "2.diff", diff)
+      }*/
+
+      if (num === "16"  || num === "17") {
+        getzprobability(reduceArr, num, "2.diff", diff)
+        getzprobability(reduceArr, num, "3.mindiff", mindiff)
+        getzprobability(reduceArr, num, "4.maxdiff", maxdiff)
+        getzprobability(reduceArr, num, "5.intv", intv)
+      }
+     
+
       if (reduceArr[num]["2.diff"][diff]) {
         diffpcnt = reduceArr[num]["2.diff"][diff]["pcnt"]
-      }
+      } //else {
+        //diffpcnt = getzprobability(reduceArr, num, "2.diff", diff)
+      //}
       if (reduceArr[num]["3.mindiff"][mindiff]) {
         mindiffpcnt = reduceArr[num]["3.mindiff"][mindiff]["pcnt"]
       }
@@ -93,6 +108,30 @@ $(function() {
       summary.push(tempobj) 
     })       
     console.log("reduceArr:", reduceArr)
+/*
+let diff = reduceArr['01']["2.diff"]
+    //console.log("diff:", diff)
+
+let diffkeys = Object.keys(diff)
+let keyarr = diffkeys.reduce((arr, num) => {
+    let obj = {}
+    if (num !="max") {
+      obj[num] = diff[num]["count"]
+      arr.push(obj)
+    }
+    return arr
+}, [])
+let xttl = 0
+keyarr.forEach(obj => {
+  for (let key in obj) {
+    xttl = xttl + key * obj[key]
+  }
+})
+let xmn = xttl/keyarr.length
+console.log("keyarr:", keyarr)
+console.log("xttl:", xttl)
+console.log("xmn:", xmn)
+*/
     let prenum649 = [{date: date, summary: summary}];
     console.log("prenum649", prenum649)
     prenum649[0].summary.sort((a, b) => b.pn - a.pn)
@@ -100,6 +139,51 @@ $(function() {
     renderTable(prenum649, prelotonum);
    })
 })
+function getzprobability(reduceArr, num, option, diff) {
+  let diffobj = reduceArr[num][option]
+  let diffkeys = Object.keys(diffobj)
+  let keyarr = diffkeys.reduce((arr, num) => {
+      let obj = {}
+      if (num !="max") {
+         obj[num] = diffobj[num]["count"]
+         arr.push(obj)
+      }
+      return arr
+  }, [])
+
+  let ttl = 0
+  let n = 0
+  keyarr.forEach(obj => {
+  for (let key in obj) {
+    ttl = ttl + (key * obj[key])
+    n = n + obj[key]
+   }
+ })
+ let mean = ttl/n
+
+ let ttls2 = 0
+ keyarr.forEach(obj => {
+  for (let key in obj) {
+    let s2 = Math.pow((key - mean),2) * obj[key]
+    ttls2 = ttls2 + s2
+   }
+ })
+ let sd = Math.sqrt(ttls2/(n-1))
+ let z = Math.abs((diff - mean))/sd
+  console.log("num", num)
+  console.log("keyarr", keyarr)
+    console.log("ttl", ttl)
+    console.log("n", n)
+
+  console.log("mean", mean)
+    console.log("ttls2", ttls2)
+    console.log("sd", sd)
+    console.log("diff", diff)
+    console.log("z", z)
+
+  return 
+}
+
 
 function getReduceArr(summaryArr) {
   console.log("summaryArr", summaryArr)
