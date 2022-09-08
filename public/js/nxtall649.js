756e0c3dd8801b9df774ee8b8b07ac8b650ef4eb
paul-juang <pauljuang3@gmail.com>
2022-09-08 15:02:13 +0800

    update nxtall539/649.js

--- public/js/nxtall649.js
+++ public/js/nxtall649.js
@@ -1,13 +1,13 @@
 //get num649.js with this program
 $(function() {
-  getNum649(loto649)  
+   let loto649 = getNum649(num649)
+   console.log("loto649", loto649)
 })
 
-function getNum649(loto649) {
-  let num649 = []
-  let filterarr = loto649.filter(function(obj) {
-    return obj["summary"];
-  })
+function getNum649(num649) {
+  let loto649 = []
+  let filterarr = num649.filter(obj => obj["date"] >"2020/01/03")
+  //let filterarr = num649.filter(obj => obj["summary"])
 
   let allDate = []
   filterarr.forEach(obj => {
@@ -15,11 +15,11 @@ function getNum649(loto649) {
   })
 
   allDate.forEach(date0 => {
-    let prevfile = loto649.filter(function(obj) {
+    let prevfile = num649.filter(function(obj) {
       return obj["date"] > date0
     })
 
-    let prelotonum = [],predate = "", prebonus = ""
+    let prelotonum = [],predate = "", prebonus
     if (prevfile.length > 0 ) {
       prelotonum = prevfile[(prevfile.length)-1]["lotonum"]
       predate = prevfile[(prevfile.length)-1]["date"]
@@ -27,7 +27,7 @@ function getNum649(loto649) {
     }
 
     if (prelotonum.length != 0  ) {
-      let arrOnChange = loto649.filter(obj => obj["date"] <= date0)
+      let arrOnChange = num649.filter(obj => obj["date"] <= date0)
       let baseArr = arrOnChange.slice(0,arrOnChange.length)
       let basefilerarr = baseArr.filter(obj => obj["summary"])
       let date = arrOnChange[0].date;
@@ -38,8 +38,8 @@ function getNum649(loto649) {
       let arr60 = arrOnChange.slice(0,60);
 
       let obj60 = getDiffnProb(arr60)
-      let objmindiff = getMindiff(arrmin)
-      let objmaxdiff = getMindiff(arrmax)
+      let objmindiff = getMinMaxdiff(arrmin)
+      let objmaxdiff = getMinMaxdiff(arrmax)
 
       let numarr = [],max = 50
       for (let i = 1; i < max ; i++) {
@@ -51,17 +51,18 @@ function getNum649(loto649) {
 
       let summary = getSummary(numarr, obj60, objmindiff, objmaxdiff, prelotonum)
       summary.sort((a,b) => a.num - b.num)
+
       let temp = {}
       temp['date'] = predate
-      temp['lotonum'] = prelotonum
       temp['bonus'] = prebonus
+      temp['lotonum'] = prelotonum
       temp['summary'] = summary
-      num649.push(temp)
+      loto649.push(temp)
     }
-    
+
   })
 
-  num649.forEach(obj => {
+  loto649.forEach(obj => {
     let summary = obj.summary
     let lotonum = obj.lotonum
     let sumarr = []
@@ -81,12 +82,103 @@ function getNum649(loto649) {
     })
     obj["summary"] = sumarr
   }) 
-  console.log("num649", num649)
+  return loto649
+
+  // functions
+  function getDiffnProb(arrofobj) {
+      let reversearr = [];   //revserse order of arrofobj elements
+      for (var i = arrofobj.length - 1; i >= 0; i--) {
+         reversearr.push(arrofobj[i]);
+      }
+
+      let arrofarr = reversearr.reduce((numarr,numobj)=> {
+        let num = numobj.lotonum;
+        numarr.push(num)
+        return numarr;
+      },[]);
+
+      let numarr = [],max = 50;
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else n = String(n);
+        numarr.push(n);
+      }
+
+      let p = 0, mean = 0,totalarr = arrofobj.length;
+      p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
+      mean = Math.round(totalarr * p);
+      let resultobj = numarr.reduce((obj,cn) => { 
+        let count = 0;
+        let position = [];
+        arrofarr.forEach((arr,index) => {
+          arr.forEach(cx => {
+            if (cx === cn) {
+              count += 1;
+              position.push(index + 1);
+            }
+          })
+        })
+
+        let tempobj = {},
+        neardistance = position.length === 0 ? totalarr+1 : totalarr - position[position.length - 1]+1,
+        deviation = count - mean;
+        tempobj["deviation"] = deviation;
+        tempobj["neardist"] = neardistance;
+        tempobj["prob"] = 1 - Math.pow(1-p,neardistance);
+        obj[cn] = tempobj;
+        return obj;
+        },{});
+        return resultobj;
+    }
+
+    function getMinMaxdiff(arrofobj) {
+      let reversearr = [];  
+      for (var i = arrofobj.length - 1; i >= 0; i--) {
+         reversearr.push(arrofobj[i]);
+      }
+
+      let arrofarr = reversearr.reduce((numarr,numobj)=> {
+        let num = numobj.lotonum;
+        numarr.push(num)
+        return numarr;
+      },[]);
+
+      let numarr = [],max = 50;
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else n = String(n);
+        numarr.push(n);
+      }
+      let p = 0, mean = 0,totalarr = arrofobj.length;
+      p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
+      mean = Math.round(totalarr * p);
+      let resultobj = numarr.reduce((obj,cn) => { 
+        let count = 0;
+        let position = [];
+        arrofarr.forEach((arr,index) => {
+          arr.forEach(cx => {
+            if (cx === cn) {
+              count += 1;
+              position.push(index + 1);
+            }
+          })
+        })
+
+        let tempobj = {},
+        deviation = count - mean;
+        tempobj["deviation"] = deviation;
+        obj[cn] = tempobj;
+        return obj;
+        },{});
+        return resultobj;
+    }
+
 }
 
 function getSummary(numarr, obj60, objmindiff, objmaxdiff) {
     let summary = [];
-    //Object.keys(reduceObj).sort((a,b)=>a-b).forEach(num => {
     numarr.forEach(num => {      
       let tempobj = {}
       let diff = obj60[num]["deviation"]
@@ -104,206 +196,6 @@ function getSummary(numarr, obj60, objmindiff, objmaxdiff) {
     })
     return summary
 }
-
-
-function getDiffnProb(arrofobj) {  
-
-    let reversearr = [];   //revserse order of arrofobj elements
-       for (var i = arrofobj.length - 1; i >= 0; i--) {
-         reversearr.push(arrofobj[i]);
-       }
-
-    let arrofarr = reversearr.reduce((numarr,numobj)=> {
-
-        let num = numobj.lotonum;
-        numarr.push(num)
-        return numarr;
-
-        },[]);
-
-
-    let numarr = [],max = 50;
-
-       for (let i = 1; i < max ; i++) {
-
-        let n = i;
-        if (n < 10) { 
-         n = "0" + n;
-        }else {
-         n = String(n);
-        }
-        numarr.push(n);
-
-      }
-
-      
-    let p = 0, mean = 0,totalarr = arrofobj.length;
-
-        p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
-
-        mean = Math.round(totalarr * p);
-
-        let resultobj = numarr.reduce((obj,cn) => {
-
-                    let count = 0;
-                    let position = [];
-                    arrofarr.forEach((arr,index) => {
-
-                      arr.forEach((cx) => {
-                        if (cx === cn) {
-                          count += 1;
-                          position.push(index + 1);
-                        }
-                      })
-
-                    })
-
-                    let tempobj = {},
-                        neardistance = position.length === 0 ? totalarr+1 : totalarr - position[position.length - 1]+1,
-                        deviation = count - mean;
-                        tempobj["deviation"] = deviation;
-                        tempobj["neardist"] = neardistance;
-                        tempobj["prob"] = 1 - Math.pow(1-p,neardistance);
-                        obj[cn] = tempobj;
-                        
-                    return obj;
-
-                 },{});
-
-       return resultobj;
-
-} //getdiffnprob
-
-function getMindiff(arrofobj) {  
-
-    let reversearr = [];   //revserse order of arrofobj elements
-       for (var i = arrofobj.length - 1; i >= 0; i--) {
-         reversearr.push(arrofobj[i]);
-       }
-
-    let arrofarr = reversearr.reduce((numarr,numobj)=> {
-
-        let num = numobj.lotonum;
-        numarr.push(num)
-        return numarr;
-
-        },[]);
-
-
-    let numarr = [],max = 50;
-
-       for (let i = 1; i < max ; i++) {
-
-        let n = i;
-        if (n < 10) { 
-         n = "0" + n;
-        }else {
-         n = String(n);
-        }
-        numarr.push(n);
-
-      }
-
-      
-    let p = 0, mean = 0,totalarr = arrofobj.length;
-
-        p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
-
-        mean = Math.round(totalarr * p);
-
-    let resultobj = numarr.reduce((obj,cn) => {
-
-                    let count = 0;
-                    let position = [];
-                    arrofarr.forEach((arr,index) => {
-
-                      arr.forEach((cx) => {
-                        if (cx === cn) {
-                          count += 1;
-                          position.push(index + 1);
-                        }
-                      })
-
-                    })
-
-                    let tempobj = {},
-                        deviation = count - mean;
-                        tempobj["deviation"] = deviation;
-                        obj[cn] = tempobj;
-                        
-                    return obj;
-
-                 },{});
-
-       return resultobj;
-
-} 
-
-
-function getMaxdiff(arrofobj) {  
-
-    let reversearr = [];   //revserse order of arrofobj elements
-       for (var i = arrofobj.length - 1; i >= 0; i--) {
-         reversearr.push(arrofobj[i]);
-       }
-
-    let arrofarr = reversearr.reduce((numarr,numobj)=> {
-
-        let num = numobj.lotonum;
-        numarr.push(num)
-        return numarr;
-
-        },[]);
-
-
-    let numarr = [],max = 50;
-
-       for (let i = 1; i < max ; i++) {
-
-        let n = i;
-        if (n < 10) { 
-         n = "0" + n;
-        }else {
-         n = String(n);
-        }
-        numarr.push(n);
-
-      }
-
-      
-    let p = 0, mean = 0,totalarr = arrofobj.length;
-
-        p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
-
-        mean = Math.round(totalarr * p);
-
-    let resultobj = numarr.reduce((obj,cn) => {
-
-                    let count = 0;
-                    let position = [];
-                    arrofarr.forEach((arr,index) => {
-
-                      arr.forEach((cx) => {
-                        if (cx === cn) {
-                          count += 1;
-                          position.push(index + 1);
-                        }
-                      })
-
-                    })
-
-                    let tempobj = {},
-                        deviation = count - mean;
-                        obj[cn] = tempobj;
-                        
-                    return obj;
-
-                 },{});
-
-       return resultobj;
-
-} 
-  
 
 
 //xxxxxxxxxxxxxxxxxxxx
