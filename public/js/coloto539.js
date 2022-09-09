2b0424f2c2d35545eaaa1bdc7ab966d48f8c0161
paul-juang <pauljuang3@gmail.com>
2022-09-09 12:22:39 +0800

    update coloto539/649.js

--- public/js/coloto539.js
+++ public/js/coloto539.js
@@ -1,5 +1,7 @@
 //print winning numbers statistic summary
 $(function() {
+
+  let loto539 = getNum539(num539)
   let filterarr = loto539.filter(function(obj) {
     return obj["summary"];
   })
@@ -107,7 +109,7 @@ function renderTable(objarr) {
        )
       .append($("<td>")
        .append($("<input>").attr({type:"text",class:"flex"}).css({textAlign:"center",fontWeight:"bold",color:colorp}).prop("readonly",true)
-         .val(obj.p))
+         .val(obj.p.toFixed(4)))
        )              
       .appendTo(tbody);
     })
@@ -116,3 +118,193 @@ function renderTable(objarr) {
   })
 
 }
+
+
+function getNum539(num539) {
+  let loto539 = []
+  let filterarr = num539.filter(obj => obj["date"] >= "2020/01/06")
+
+  let allDate = []
+  filterarr.forEach(obj => allDate.push(obj.date))
+
+  allDate.forEach(date0 => {
+    let prevfile = num539.filter(obj => obj["date"] > date0)
+
+    let prelotonum = [],predate = ""
+    if (prevfile.length > 0 ) {
+      prelotonum = prevfile[(prevfile.length)-1]["lotonum"]
+      predate = prevfile[(prevfile.length)-1]["date"]
+    }
+
+    if (prelotonum.length != 0  ) {
+      let arrOnChange = num539.filter(obj => obj["date"] <= date0)
+      let baseArr = arrOnChange.slice(0,arrOnChange.length)
+      let basefilerarr = baseArr.filter(obj => obj["summary"])
+      let date = arrOnChange[0].date;
+      let lotonum = arrOnChange[0].lotonum
+      let minrecords = 26;
+      let arrmax = arrOnChange.slice(0,arrOnChange.length);
+      let arrmin = arrOnChange.slice(0,arrOnChange.length - minrecords);
+      let arr60 = arrOnChange.slice(0,60);
+
+      let obj60 = getDiffnProb(arr60)
+      let objmindiff = getMinMaxdiff(arrmin)
+      let objmaxdiff = getMinMaxdiff(arrmax)
+
+      let numarr = [],max = 40
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else  n = String(n);
+        numarr.push(n);
+      }
+
+      let summary = getSummary(numarr, obj60, objmindiff, objmaxdiff, prelotonum)
+      summary.sort((a,b) => a.num - b.num)
+
+      let temp = {}
+      temp['date'] = predate
+      temp['lotonum'] = prelotonum
+      temp['summary'] = summary
+      loto539.push(temp)
+    }
+
+  })
+
+  loto539.forEach(obj => {
+    let summary = obj.summary
+    let lotonum = obj.lotonum
+    let sumarr = []
+    lotonum.forEach(num => {
+      let sumobj = {}
+      summary.forEach(obj0 => {
+        if (num === obj0.num) {
+          sumobj["num"] = obj0.num
+          sumobj["diff"] = obj0.diff
+          sumobj["mindiff"] = obj0.mindiff
+          sumobj["maxdiff"] = obj0.maxdiff
+          sumobj["intv"] = obj0.intv
+          sumobj["p"] = obj0.p
+        }
+      })
+      sumarr.push(sumobj)
+    })
+    obj["summary"] = sumarr
+  }) 
+  let therest = num539.slice(loto539.length)
+  loto539 = [...loto539, ...therest]
+
+  return loto539
+
+  // functions
+  function getDiffnProb(arrofobj) {
+      let reversearr = [];   //revserse order of arrofobj elements
+      for (var i = arrofobj.length - 1; i >= 0; i--) {
+         reversearr.push(arrofobj[i]);
+      }
+
+      let arrofarr = reversearr.reduce((numarr,numobj)=> {
+        let num = numobj.lotonum;
+        numarr.push(num)
+        return numarr;
+      },[]);
+
+      let numarr = [],max = 40;
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else n = String(n);
+        numarr.push(n);
+      }
+
+      let p = 0, mean = 0,totalarr = arrofobj.length;
+      p = 1/39 + 1/38 + 1/37 + 1/36 + 1/35;
+      mean = Math.round(totalarr * p);
+      let resultobj = numarr.reduce((obj,cn) => { 
+        let count = 0;
+        let position = [];
+        arrofarr.forEach((arr,index) => {
+          arr.forEach(cx => {
+            if (cx === cn) {
+              count += 1;
+              position.push(index + 1);
+            }
+          })
+        })
+
+        let tempobj = {},
+        neardistance = position.length === 0 ? totalarr+1 : totalarr - position[position.length - 1]+1,
+        deviation = count - mean;
+        tempobj["deviation"] = deviation;
+        tempobj["neardist"] = neardistance;
+        tempobj["prob"] = 1 - Math.pow(1-p,neardistance);
+        obj[cn] = tempobj;
+        return obj;
+        },{});
+        return resultobj;
+    }
+
+    function getMinMaxdiff(arrofobj) {
+      let reversearr = [];  
+      for (var i = arrofobj.length - 1; i >= 0; i--) {
+         reversearr.push(arrofobj[i]);
+      }
+
+      let arrofarr = reversearr.reduce((numarr,numobj)=> {
+        let num = numobj.lotonum;
+        numarr.push(num)
+        return numarr;
+      },[]);
+
+      let numarr = [],max = 40;
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else n = String(n);
+        numarr.push(n);
+      }
+      let p = 0, mean = 0,totalarr = arrofobj.length;
+      p = 1/39 + 1/38 + 1/37 + 1/36 + 1/35;
+      mean = Math.round(totalarr * p);
+      let resultobj = numarr.reduce((obj,cn) => { 
+        let count = 0;
+        let position = [];
+        arrofarr.forEach((arr,index) => {
+          arr.forEach(cx => {
+            if (cx === cn) {
+              count += 1;
+              position.push(index + 1);
+            }
+          })
+        })
+
+        let tempobj = {},
+        deviation = count - mean;
+        tempobj["deviation"] = deviation;
+        obj[cn] = tempobj;
+        return obj;
+        },{});
+        return resultobj;
+    }
+
+    function getSummary(numarr, obj60, objmindiff, objmaxdiff) {
+      let summary = [];
+      numarr.forEach(num => {      
+      let tempobj = {}
+      let diff = obj60[num]["deviation"]
+      let intv = obj60[num]["neardist"];
+      let p = obj60[num]["prob"];    
+      let mindiff = objmindiff[num]["deviation"];
+      let maxdiff = objmaxdiff[num]["deviation"];
+      tempobj['num'] = num;
+      tempobj['diff'] = diff;
+      tempobj['mindiff'] = mindiff;
+      tempobj['maxdiff'] = maxdiff;
+      tempobj['intv'] = intv;
+      tempobj['p'] = p;
+      summary.push(tempobj)
+    })
+     return summary    
+   }
+}
+
--- public/js/coloto649.js
+++ public/js/coloto649.js
@@ -1,5 +1,6 @@
 //print winning numbers statistic summary
 $(function() {
+  let loto649 = getNum649(num649)
   let filterarr = loto649.filter(function(obj) {
     return obj["summary"];
   })
@@ -107,7 +108,7 @@ function renderTable(objarr) {
        )
       .append($("<td>")
        .append($("<input>").attr({type:"text",class:"flex"}).css({textAlign:"center",fontWeight:"bold",color:colorp}).prop("readonly",true)
-         .val(obj.p))
+         .val(obj.p.toFixed(4)))
        )              
       .appendTo(tbody);
     })
@@ -116,3 +117,193 @@ function renderTable(objarr) {
   })
 
 }
+
+function getNum649(num649) {
+  let loto649 = []
+  let filterarr = num649.filter(obj => obj["date"] >="2019/12/31")
+
+  let allDate = []
+  filterarr.forEach(obj => allDate.push(obj.date))
+
+  allDate.forEach(date0 => {
+    let prevfile = num649.filter(obj => obj["date"] > date0)
+
+    let prelotonum = [],predate = "", prebonus
+    if (prevfile.length > 0 ) {
+      prelotonum = prevfile[(prevfile.length)-1]["lotonum"]
+      predate = prevfile[(prevfile.length)-1]["date"]
+      prebonus = prevfile[(prevfile.length)-1]["bonus"]
+    }
+
+    if (prelotonum.length != 0  ) {
+      let arrOnChange = num649.filter(obj => obj["date"] <= date0)
+      let baseArr = arrOnChange.slice(0,arrOnChange.length)
+      let basefilerarr = baseArr.filter(obj => obj["summary"])
+      let date = arrOnChange[0].date;
+      let lotonum = arrOnChange[0].lotonum
+      let minrecords = 108;
+      let arrmax = arrOnChange.slice(0,arrOnChange.length);
+      let arrmin = arrOnChange.slice(0,arrOnChange.length - minrecords);
+      let arr60 = arrOnChange.slice(0,60);
+
+      let obj60 = getDiffnProb(arr60)
+      let objmindiff = getMinMaxdiff(arrmin)
+      let objmaxdiff = getMinMaxdiff(arrmax)
+
+      let numarr = [],max = 50
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else  n = String(n);
+        numarr.push(n);
+      }
+
+      let summary = getSummary(numarr, obj60, objmindiff, objmaxdiff, prelotonum)
+      summary.sort((a,b) => a.num - b.num)
+
+      let temp = {}
+      temp['date'] = predate
+      temp['bonus'] = prebonus
+      temp['lotonum'] = prelotonum
+      temp['summary'] = summary
+      loto649.push(temp)
+    }
+
+  })
+
+  loto649.forEach(obj => {
+    let summary = obj.summary
+    let lotonum = obj.lotonum
+    let sumarr = []
+    lotonum.forEach(num => {
+      let sumobj = {}
+      summary.forEach(obj0 => {
+        if (num === obj0.num) {
+          sumobj["num"] = obj0.num
+          sumobj["diff"] = obj0.diff
+          sumobj["mindiff"] = obj0.mindiff
+          sumobj["maxdiff"] = obj0.maxdiff
+          sumobj["intv"] = obj0.intv
+          sumobj["p"] = obj0.p
+        }
+      })
+      sumarr.push(sumobj)
+    })
+    obj["summary"] = sumarr
+  })
+  let therest = num649.slice(loto649.length)
+  loto649 = [...loto649, ...therest]
+  return loto649
+
+  // functions
+  function getDiffnProb(arrofobj) {
+      let reversearr = [];   //revserse order of arrofobj elements
+      for (var i = arrofobj.length - 1; i >= 0; i--) {
+         reversearr.push(arrofobj[i]);
+      }
+
+      let arrofarr = reversearr.reduce((numarr,numobj)=> {
+        let num = numobj.lotonum;
+        numarr.push(num)
+        return numarr;
+      },[]);
+
+      let numarr = [],max = 50;
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else n = String(n);
+        numarr.push(n);
+      }
+
+      let p = 0, mean = 0,totalarr = arrofobj.length;
+      p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
+      mean = Math.round(totalarr * p);
+      let resultobj = numarr.reduce((obj,cn) => { 
+        let count = 0;
+        let position = [];
+        arrofarr.forEach((arr,index) => {
+          arr.forEach(cx => {
+            if (cx === cn) {
+              count += 1;
+              position.push(index + 1);
+            }
+          })
+        })
+
+        let tempobj = {},
+        neardistance = position.length === 0 ? totalarr+1 : totalarr - position[position.length - 1]+1,
+        deviation = count - mean;
+        tempobj["deviation"] = deviation;
+        tempobj["neardist"] = neardistance;
+        tempobj["prob"] = 1 - Math.pow(1-p,neardistance);
+        obj[cn] = tempobj;
+        return obj;
+        },{});
+        return resultobj;
+    }
+
+    function getMinMaxdiff(arrofobj) {
+      let reversearr = [];  
+      for (var i = arrofobj.length - 1; i >= 0; i--) {
+         reversearr.push(arrofobj[i]);
+      }
+
+      let arrofarr = reversearr.reduce((numarr,numobj)=> {
+        let num = numobj.lotonum;
+        numarr.push(num)
+        return numarr;
+      },[]);
+
+      let numarr = [],max = 50;
+      for (let i = 1; i < max ; i++) {
+        let n = i;
+        if (n < 10) n = "0" + n;
+        else n = String(n);
+        numarr.push(n);
+      }
+      let p = 0, mean = 0,totalarr = arrofobj.length;
+      p = 1/49 + 1/48 + 1/47 + 1/46 + 1/45 + 1/44;
+      mean = Math.round(totalarr * p);
+      let resultobj = numarr.reduce((obj,cn) => { 
+        let count = 0;
+        let position = [];
+        arrofarr.forEach((arr,index) => {
+          arr.forEach(cx => {
+            if (cx === cn) {
+              count += 1;
+              position.push(index + 1);
+            }
+          })
+        })
+
+        let tempobj = {},
+        deviation = count - mean;
+        tempobj["deviation"] = deviation;
+        obj[cn] = tempobj;
+        return obj;
+        },{});
+        return resultobj;
+    }
+
+    function getSummary(numarr, obj60, objmindiff, objmaxdiff) {
+      let summary = [];
+      numarr.forEach(num => {      
+      let tempobj = {}
+      let diff = obj60[num]["deviation"]
+      let intv = obj60[num]["neardist"];
+      let p = obj60[num]["prob"];    
+      let mindiff = objmindiff[num]["deviation"];
+      let maxdiff = objmaxdiff[num]["deviation"];
+      tempobj['num'] = num;
+      tempobj['diff'] = diff;
+      tempobj['mindiff'] = mindiff;
+      tempobj['maxdiff'] = maxdiff;
+      tempobj['intv'] = intv;
+      tempobj['p'] = p;
+      summary.push(tempobj)
+    })
+     return summary    
+   }
+}
+
